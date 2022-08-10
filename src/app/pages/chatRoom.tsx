978d62f56e9e3b25d49e9b865fd60833e51ce959
osaretin frank <osaretin.frank10@gmail.com>
2022-08-10 10:39:46 +0100

    implemented user Logout

--- src/app/pages/chatRoom.tsx
+++ src/app/pages/chatRoom.tsx
@@ -1,5 +1,28 @@
 import React from "react"
+import { Box, Button, Typography, TextField } from "@mui/material"
+import { Logout, Message, Send } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { LogOut } from "../../features/chatRoom.tsx/user"
 
 export const ChatRoom = () => {
-  return <div>chatRoom</div>
+  const dispatch = useDispatch()
+  return (
+    <Box className="chat-room">
+      <header className="chat-room_header">
+        <Typography variant="h6">
+          WebChats - Chat Room <Message />
+        </Typography>
+        <Button
+          startIcon={<Logout />}
+          variant="contained"
+          color="error"
+          onClick={() => {
+            dispatch({ type: LogOut.type })
+          }}
+        >
+          Sign Out
+        </Button>
+      </header>
+    </Box>
+  )
 }
