91ae5e77a7b1c15206c2e7b1615f323856071def
osaretin frank <osaretin.frank10@gmail.com>
2022-08-10 08:42:06 +0100

    implemented userSignIn

--- src/app/pages/Join.tsx
+++ src/app/pages/Join.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useState, FormEvent } from "react"
 import { Button, TextField, Box, Typography, Container } from "@mui/material"
 import { Login } from "@mui/icons-material"
 import chatSvg from "../assets/svgs/chat.svg"
+import { useDispatch } from "react-redux"
+import { Join as JoinChat } from "../../features/chatRoom.tsx/user"
+import { useNavigate } from "react-router-dom"
 
 export const Join = () => {
+  const [userID, setUserID] = useState<string | null>(null)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    dispatch({ type: JoinChat.type, payload: { userID } })
+    navigate("/chat-room")
+  }
   return (
     <Container>
       <Box sx={{ marginTop: 20 }}>
@@ -14,10 +25,18 @@ export const Join = () => {
           If it is your first time, You'll be added to the chat room.
         </Typography>
 
-        <form style={{ marginTop: "2.5rem" }}>
-          <TextField placeholder="Enter user ID" focused label="UserID" />
+        <form style={{ marginTop: "2.5rem" }} onSubmit={onSubmit}>
+          <TextField
+            placeholder="Enter user ID"
+            focused
+            label="UserID"
+            value={userID}
+            onChange={(e) => {
+              setUserID(e.target.value)
+            }}
+          />
           <div style={{ marginTop: "0.7rem" }}>
-            <Button startIcon={<Login />} variant="contained">
+            <Button startIcon={<Login />} variant="contained" type="submit">
               Join
             </Button>
           </div>
